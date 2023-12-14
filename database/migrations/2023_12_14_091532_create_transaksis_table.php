b42be57d8a7907e655d4bee63260d9f3d321e234
marcellaav <patriciamarcellaav@gmail.com>
2023-12-14 17:10:56 +0700

    migration transaksi

---
+++ database/migrations/2023_12_14_091532_create_transaksis_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksis', function (Blueprint $table) {
+            $table->id('id_transaksi');
+            $table->foreignId('id_user')->constrained('users', 'id_user');
+            $table->date('tgl_transaksi');
+            $table->integer('total_transaksi');
+            $table->integer('denda');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksis');
+    }
+};
