b8c69a76cf6955aa78bb6c4e54b8f33fb8ad2441
zeddvyy <vanayalerrick@gmail.com>
2023-12-20 18:01:45 +0700

    style user page

--- resources/views/User/ulasan/ulasan.blade.php
+++ resources/views/User/ulasan/ulasan.blade.php
@@ -9,15 +9,84 @@
     </head>
 
     <style>
-        body {
-            background-color: #F8F7FC;
-        }
-    </style>
+    /* Gaya Kartu Ulasan */
+    .testimonial-card {
+        border: none;
+        border-radius: 15px;
+        overflow: hidden;
+        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+        transition: transform 0.3s ease-in-out;
+        background-color: #fff;
+        margin-bottom: 20px;
+    }
 
-    <div class="container-details">
-        <h1 class="fw-bold my-4 text-center">
-            Ulasan
-        </h1>
+    .testimonial-card:hover {
+        transform: translateY(-5px);
+    }
+
+    .testimonial-card blockquote {
+        padding: 20px;
+        height: 100%; /* Sesuaikan tinggi dengan kebutuhan desain */
+        margin: 0;
+        border-left: 12px solid #003EB7; /* Warna batas kiri */
+        display: flex;
+        align-items: center; /* Menengahkan isi dalam blockquote */
+    }
+
+    .testimonial-card blockquote p {
+        margin: 0; /* Hapus margin default */
+    }
+
+ 
+    .testimonial-card .v-card {
+        padding: 40px;
+        /* background-color: #f9f9f9; */
+    } 
+
+    .testimonial-card img {
+        width: 60px;
+        height: 60px;
+        border-radius: 50%;
+        object-fit: cover;
+        border: 1px solid #fff; /* Warna pinggiran foto */
+    }
+
+    .testimonial-card .author-name {
+        margin-left: 4px;
+    }
+
+    .testimonial-card .author-name span {
+        display: block;
+        font-weight: bold;
+        color: #003EB7; /* Warna nama pengguna */
+    }
+
+    .rating {
+        margin-top: 10px;
+    }
+
+    .rating p {
+        display: inline-block;
+        padding: 5px 10px;
+        border-radius: 20px;
+        font-weight: bold;
+    }
+
+    .delete-button {
+        border: none;
+        background: none;
+        cursor: pointer;
+        outline: none;
+        font-size: 18px;
+    }
+
+    .delete-button:hover {
+        color: red;a
+    }
+</style>
+
+<div class="container-details">
+        <h1 class="fw-bold my-4 text-center">Ulasan</h1>
 
         @if (session('success'))
             <div class="alert alert-success mt-4 text-center" id="alerts">
@@ -41,56 +110,57 @@
                 </div>
             </div>
         @endauth
+
         <div class="col align-items-center-mt-4">
             @if ($ulasan->isEmpty())
                 <div class="col-flex mt-4">
                     <p class="text-center h5"><strong>Ulasan tidak ada</strong></p>
                 </div>
             @endif
-            @foreach ($ulasan as $item)
-                <div class="row-lg-flex row-md-flex rw-sm-flex mt-4">
-                    <div class="card w-100 p-2">
-                        <div class="card-body">
-                            <div class="col d-flex m-2">
-                                <div class="me-auto d-flex align-items-center">
-                                    @if ($item->user->photo == null)
-                                        <img src="{{ asset('images/profilepic.png') }}" alt=""
-                                            class="image-fluid rounded-circle" width="32" height="32"
-                                            style="object-fit: cover">
-                                    @else
-                                        <img src="{{ asset('storage/profileUser/' . $item->user->photo) }}" alt=""
-                                            class="image-fluid rounded-circle" width="32" height="32"
-                                            style="object-fit: cover">
-                                    @endif
-                                    <p class="ms-2 card-text fs-5">{{ $item->user->nama }}</p>
-                                </div>
-                                <p class="ms-2 card-text fs-6">{{ $item['tanggal'] }}</p>
-                            </div>
-                            <div class="col d-flex m-2">
-                                <p
-                                    class="
-                                @if ($item['rating'] == 'Baik') text-success
-                                @elseif ($item['rating'] == 'Cukup')
-                                    text-warning
-                                @else
-                                    text-danger @endif
-                                card-text fw-bold">
-                                    {{ $item['rating'] }}</p>
-                            </div>
-                            <div class="col d-flex m-2">
-                                <p class="card-text">{{ $item['ulasan'] }}</p>
+
+            <div class="container">
+                <div class="row">
+                    @foreach ($ulasan as $item)
+                        <div class="col-lg-4 mb-4">
+                            <div class="testimonial-card">
+                            <div class="rating d-flex justify-content-between align-items-center">
+                                <p class="@if ($item['rating'] == 'Baik') text-success @elseif ($item['rating'] == 'Cukup') text-warning @else text-danger @endif fw-bold">{{ $item['rating'] }}</p>
+                                
                                 @auth
                                     @if ($item['id_user'] == Auth::user()->id_user)
-                                        <button type="button" class="btn ms-auto delete-button" data-bs-toggle="modal"
-                                            data-bs-target="#confirmDelete" data-index="{{ $item->id_ulasan }}"><i
-                                                class="fa fa-trash fs-5" style="color: red"></i></button>
+                                        <button type="button" class="btn delete-button" data-bs-toggle="modal"
+                                            data-bs-target="#confirmDelete" data-index="{{ $item->id_ulasan }}">
+                                            <i class="fa fa-trash" style="color: red"></i>
+                                        </button>
                                     @endif
                                 @endauth
                             </div>
+
+                                <div class="d-flex v-card align-items-center">
+                                    @if ($item->user->photo == null)
+                                        <img src="{{ asset('images/profilepic.png') }}" alt="Image"
+                                            class="img-fluid rounded-circle" width="60" height="60"
+                                            style="object-fit: cover">
+                                    @else
+                                        <img src="{{ asset('storage/profileUser/' . $item->user->photo) }}"
+                                            alt="Image" class="img-fluid rounded-circle" width="60" height="60"
+                                            style="object-fit: cover">
+                                    @endif
+
+                                    <div class="author-name">
+                                        <span>{{ $item->user->nama }}</span>
+                                    </div>
+                                    <p class="ms-auto card-text fs-6">{{ $item['tanggal'] }}</p>
+                                </div>
+                                <blockquote>
+                                    <p>{{ $item['ulasan'] }}</p>
+                                </blockquote>
+                            </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
-            @endforeach
+            </div>
+
             {{-- pagination --}}
             <div class="mt-2 d-flex justify-content-center">
                 {!! $ulasan->links() !!}
@@ -120,27 +190,26 @@
                         </div>
                         <div class="form-group mb-2">
                             <label for="ulasan">Ulasan</label>
-                            <textarea class="form-control" id="ulasan" rows="3" name="ulasan" style="min-height: 100px;"></textarea>
+                            <textarea class="form-control" id="ulasan" rows="3" name="ulasan"
+                                style="min-height: 100px;"></textarea>
                         </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                            <button type="submit" class="btn btn-primary" style="background-color: #003EB7">Simpan</button>
+                        </div>
+                    </form>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                    <button type="submit" class="btn btn-primary" style="background-color: #003EB7">Simpan</button>
-                </div>
-                </form>
             </div>
         </div>
     </div>
 
-    {{-- modal confirm hapus ulasan --}}
+    {{-- modal konfirmasi hapus ulasan --}}
     <div class="modal fade" id="confirmDelete" tabindex="-1" role="dialog" aria-labelledby="confimDeleteTitle"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-body align-content-center">
-                    <p class="text-center mb-0">
-                        Apakah Anda yakin ingin manghapus ulasan?
-                    </p>
+                    <p class="text-center mb-0">Apakah Anda yakin ingin manghapus ulasan?</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
@@ -155,24 +224,21 @@
         </div>
     </div>
 
-
     <script>
-        setTimeout(function() {
+        setTimeout(function () {
             var alert = document.getElementById('alerts');
             if (alert) {
                 alert.style.transition = 'opacity 0.5s';
                 alert.style.opacity = 0;
 
-                setTimeout(function() {
+                setTimeout(function () {
                     alert.remove();
                 }, 500);
             }
         }, 3500);
-    </script>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            $('.delete-button').on('click', function() {
+        document.addEventListener('DOMContentLoaded', function () {
+            $('.delete-button').on('click', function () {
                 var index = $(this).data('index');
                 console.log('Index:', index); // debug only
                 $('#indexUlasan').val(index);
